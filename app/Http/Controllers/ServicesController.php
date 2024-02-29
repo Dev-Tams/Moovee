c17449a34fa81f1529a2ea3de4f1627d7a69124f
Dev-Tams <tammyalemu@gmail.com>
2024-02-29 05:59:14 +0100

    updated service controller

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -2,21 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Services;
 use Illuminate\Http\Request;
+
 
 class ServicesController extends Controller
 {
     //
 
-    public function index(){
+    public function index()
+    {
         return view('services/index');
     }
 
-    public function show(){
+    public function show()
+    {
         return view('services/show');
     }
 
-    public function store(){
-       
+    public function store(Request $request)
+    {
+        // dd(request()->all());
+
+        //create a new quote using the db 
+        //save to the db
+        //redirect
+        $validatedData =$request->validate([
+            'name'=> 'required|string|min:2|max:50',
+            'email'=> 'required|string|min:6|max:255|email',
+            'service-type' => 'required|string|max:255',
+            'pickup-location' => 'required|string',
+            'dropoff-location' => 'required|string',
+            'date' => 'required|date',
+            'time' => 'required|date_format:H:i', 
+            'weight-desc' => 'nullable|string',
+            'accept_terms' => 'exclude|required|accepted'
+        ]);
+
+
+        $quote = new Services();
+        $quote->name = $validatedData['name'];
+        $quote->email = $validatedData['email'];
+        $quote->phone = $validatedData['phone'];
+        // Assign other fields as well
+        $quote->save();
+
     }
 }
--- database/migrations/2024_02_27_090819_create_services_table.php
+++ database/migrations/2024_02_27_090819_create_services_table.php
@@ -13,6 +13,24 @@ return new class extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('service_type');
+            $table->string('pickup_street_address');
+            $table->string('pickup_landmark');
+            $table->string('pickup_city');
+            $table->string('pickup_region')->nullable();
+            $table->string('pickup_state');
+            $table->string('pickup_country');
+            $table->string('dropoff_street_address');
+            $table->string('dropoff_landmark');
+            $table->string('dropoff_city');
+            $table->string('dropoff_region')->nullable();
+            $table->string('dropoff_state');
+            $table->string('dropoff_country');
+            $table->date('date');
+            $table->time('time');
+            $table->text('weight_desc')->nullable();
             $table->timestamps();
         });
     }
