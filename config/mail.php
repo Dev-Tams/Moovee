187693046130b14e1329ed2d78154e38c88592ce
Dev-Tams <tammyalemu@gmail.com>
2024-04-04 20:31:06 +0100

    Just tackled a pesky email connection issue! 😂 Switched SMTP port to 465 for development, added authorized user emails now lets go  testing on Mailgun

--- config/mail.php
+++ config/mail.php
@@ -38,12 +38,12 @@ return [
             'transport' => 'smtp',
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
+            'port' => env('MAIL_PORT', 465, true),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN'),
+            'local_domain' => env('MAIL_EHLO_DOMAIN', 'mydomain.com'),
         ],
 
         'ses' => [
