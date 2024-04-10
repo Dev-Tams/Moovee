6a497c3070bc1ee3f9e0763151290de9653a61b0
Dev-Tams <tammyalemu@gmail.com>
2024-04-10 05:45:16 +0100

    added the js asset with vite, frontend config

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,2 @@
 import './bootstrap';
+import '../../public/js/signin';
--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/FE/css/login.css">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/login.css') }}">
+    @vite(['resources/js/app.js'])
     <title>Moove sign In</title>
 </head>
 
