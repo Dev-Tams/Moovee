23fea3e46f7fa07496443482ce8c12ff2c3a9216
Dev-Tams <tammyalemu@gmail.com>
2024-04-28 00:19:25 +0100

    styling the contact form

--- resources/views/Static/Contact.blade.php
+++ resources/views/Static/Contact.blade.php
@@ -9,26 +9,29 @@
 </head>
 
 <body class="mt-4">
-    <div class="md: container md:mx-auto flex items-center flex-col">
-        <h1> Welcome to Moove logistics. </h1>
+    <div class="md: container md:mx-auto flex items-center flex-col bg-gray-100">
+        <h1 class="mb-8"> Welcome to Moove logistics. </h1>
 
-        <h2> Want to get in touch with us?</h2>
+        <h2 class="mb-0"> Want to get in touch with us?</h2>
 
+        <section class="bg-white rounded-lg mx-auto max-w-lg my-4 p-4 sm:p-10">
 
-        <section>
             <form action="#" method="POST" class="box-border ">
                 <div>
-                    <input type="text" name="name" placeholder="Input your name" value="{{old('name')}}" class="text-base mt-4 h-12 w-64 border border-black rounded-md px-4">
+                    <label for="Name" class="block font-bold text-lg mb-0"> Your Name</label>
+                    <input type="text" name="name" placeholder="Input your name" value="{{old('name')}}" class="text-base mt-0 h-12 w-64 border border-black rounded-md px-4">
                 </div>
 
-                <div>
-                    <input type="email" name="email" placeholder="Provide a valid email address" value="{{old('email')}}" class=" text-base mt-4 h-12 w-64 rounded-md px-4">
+                <div class="mb-2">
+                    <label for="email" class="block font-bold text-lg mb-0 mt-4"> Your email address</label>
+                    <input type="email" name="email" placeholder="Provide a valid email address" value="{{old('email')}}" class=" text-base mt-0 h-12 w-64 rounded-md px-4">
                 </div>
 
-                <textarea name="message" id="" cols="30" rows="10" placeholder="What's your message?" value="{{old('message')}}" class="mt-4"></textarea>
+                <textarea name="message" id="" rows="5" placeholder="What's your message?" value="{{old('message')}}" class="mt-4 border-gray-300 p-3 w-full rounded-md
+                 text-base resize-y" style="width: 267px; height: 157px;"></textarea>
 
                 <div>
-                    <input type="submit" value="submit" class="text-base mt-4 h-12 w-64 rounded-md px-4 shadow-sm text-black hover:text-white font-bold">
+                    <input type="submit" value="submit" class="text-base mt-4 ml-14 h-12 w-52 rounded-md px-4 shadow-sm text-black hover:text-white hover:bg-black font-bold">
                 </div>
             </form>
         </section>
