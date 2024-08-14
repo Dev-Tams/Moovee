2bf89a38b76ea5ef495c4874a62330bb833215bd
Dev-Tams <tammyalemu@gmail.com>
2024-08-14 18:45:52 +0100

    refactored controller

--- routes/web.php
+++ routes/web.php
@@ -23,105 +23,43 @@ use Illuminate\Foundation\Auth\EmailVerificationRequest;
 */
 
 
-//using the --invoke method, simply dedicates a single action to a controller
+Route::controller(StaticController::class)->group( function () {
+  Route::get('/', 'index');
+  Route::get('/contact', 'contact');
+  Route::get('/about', 'about');
+  Route::get('/terms', 'terms');
+  Route::post('/contact', 'store');
+});
 
 
 
-//|--------------------------------------------------------------------------
-//| Static Pages
-//|--------------------------------------------------------------------------
-Route::get('/', [StaticController::class, 'index']);
-Route::get('/contact',  [StaticController::class, 'contact']);
-Route::get('/about',  [StaticController::class, 'about']);
-Route::view('/terms', 'terms');
-Route::view('footer', 'footer');
-
-Route::post('/contact', [StaticController::class, 'store'])->name('contact.store');
+Route::controller(ServiceController::class)->group( function (){
+    Route::get('quote', 'quote');
+    Route::get('/service', 'index');
+    Route::get('service/book', 'create')->middleware('auth');
+    Route::post('service/book', 'store')->name('service.store')->middleware('auth');
+    Route::get('service/orders', 'show')->middleware('auth');
+    Route::get('service/{service}', 'manage')->middleware('auth');
+});
 
 
 
-//|--------------------------------------------------------------------------
-//| Service routes
-//|--------------------------------------------------------------------------
-Route::get('quote', [ServiceController::class, 'quote']);
-Route::get('/service', [ServiceController::class, 'index'])->name('service.index');
-Route::get('service/book', [ServiceController::class, 'create'])->middleware('auth')->name('service.create');
-Route::post('service/book', [ServiceController::class, 'store'])->middleware('auth')->name('service.store');
-//confirm booked service by mail
-Route::view('/mail',  'serviceConfirmed');
-
-
-//|--------------------------------------------------------------------------
-//| Order routes
-//|--------------------------------------------------------------------------
-
-//Show, edit and delete orders
-Route::get('/orders', [ServiceController::class, 'show'])->middleware('auth');
-Route::get('services/{id}', [ServiceController::class, 'manage'])->middleware('auth');
+Route::controller(UserController::class)->group( function (){
+    Route::get('/register', 'create')->name('register.storee');
+    Route::post('/register','store')->name('register.stree');
+})->middleware('guest');
 
 
 
-//testing the tailwind css class
-Route::view('/app', 'app');
+Route::controller(LoginController::class)->group( function (){
+    Route::get('/login', 'create')->middleware('guest')->name('login');
+    Route::post('/login', 'authenticate')->middleware('guest');
+    Route::post('/logout', 'logout')->middleware('auth');
+});
 
 
-
-//|--------------------------------------------------------------------------
-//| User registration
-//|--------------------------------------------------------------------------
-//Handles a new user registration 
-Route::get('/register', [UserController::class, 'create'])->middleware('guest')->name('register.storee');
-
-// get request to verify email
-Route::get('/email/verify', function () {
-    return view('users.verify-email');
-})->middleware('auth')->name('verification.notice');
-
-//clickable verifiable link
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
-
-    return redirect('/home');
-})->middleware(['auth', 'signed'])->name('verification.verify');
-
-//re-request email verify link, either broken or deleted
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
-
-    return back()->with('message', 'Verification link sent!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
-//login user, if mail is verified
-Route::post('/register', [UserController::class, 'store'])->middleware('guest')->name('register.stree');
-
-
-
-
-
-//|--------------------------------------------------------------------------
-//| AUthenticate existing users
-//|--------------------------------------------------------------------------
-//Handles auth users 
-Route::get('/login', [LoginController::class, 'create'])->middleware('guest')->name('login');
-Route::post('/login', [LoginController::class, 'authenticate'])->middleware('guest');
-Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth');
-
-
-//|--------------------------------------------------------------------------
-//| Route for important changes (requires password)
-//|--------------------------------------------------------------------------
 Route::get('/confirm-password', function () {
     return view('users.confirm-password');
 })->middleware('auth')->name('password.confirm');
-// Checking passowrds (Post)
-// Route::post('/confirm-password', function (Request $request) {
-//     if (!Hash::check($request->password, $request->user()->password)) {
-//         return back()->withErrors([
-//             'password' => ['The provided password does not match our records.']
-//         ]);
-//     }
 
-//     $request->session()->passwordConfirmed();
-
-//     return redirect()->intended();
-// })->middleware(['auth', 'throttle:6,1']);
 
